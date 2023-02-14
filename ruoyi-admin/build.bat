a9f29fc055189e55c301fe9b997c257cf6e85696
陈德华 <hua@131.im>
2023-02-14 14:05:02 +0800

    * update

---
+++ ruoyi-admin/build.bat
@@ -0,0 +1,2 @@
+docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:ms-admin .
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:ms-admin
---
+++ ruoyi-ui/build.bat
@@ -0,0 +1,2 @@
+docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:nginx-web .
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:nginx-web
