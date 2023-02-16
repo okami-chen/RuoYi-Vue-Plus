02a02070ad7f560698e04a6e247faa32e08cd70d
陈德华 <hua@131.im>
2023-02-16 10:42:24 +0800

    * update

---
+++ ruoyi-extend/ruoyi-monitor-admin/build.bat
@@ -0,0 +1,2 @@
+docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:ms-monitor .
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:ms-monitor
---
+++ ruoyi-extend/ruoyi-xxl-job-admin/build.bat
@@ -0,0 +1,2 @@
+docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:ms-xxl .
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:ms-xxl
